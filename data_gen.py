efac133db6fcf3d4d874fc1aeb6fb5eff347d8e2
MitchellJC <81349046+MitchellJC@users.noreply.github.com>
2023-08-29 17:01:49 +1000

    Created generate data function.
    For easier experiments of multiple trials.

--- data_gen.py
+++ data_gen.py
@@ -54,66 +54,80 @@ HOSPITAL_HEADER = ('hospital_id', 'day', 'open_minutes', 'hospital_open_cost',
 random.seed(SEED)
 np.random.seed(SEED)
 
-for num_or in NUM_ORS:
-    print(f"Generating data for {num_or} operating rooms")
-    for num_patients in NUM_PATIENTS:
-        print(f"\tGenerating data for {num_patients} patients")
-        # Create sets
-        P = range(num_patients)
-        H = range(NUM_HOSPITALS)
-        R = range(num_or)
-        D = range(NUM_DAYS)
+def generate_data(num_patients, num_or, seed=None):
+    if seed:
+        random.seed(seed)
+        np.random.seed(seed)
+    
+    # Create sets
+    P = range(num_patients)
+    H = range(NUM_HOSPITALS)
+    R = range(num_or)
+    D = range(NUM_DAYS)
+    
+    # Create urgency data
+    rho = randint.rvs(URGENT_LOW, URGENT_HIGH + 1, size=(num_patients, 1))
+    
+    # Create wait time data
+    alpha = randint.rvs(ELAPSED_WAIT_LOW, ELAPSED_WAIT_HIGH + 1, 
+                        size=(num_patients, 1))
+    
+    # Create health status data
+    health_status = np.multiply(rho, NUM_DAYS - alpha) 
         
-        # Create urgency data
-        rho = randint.rvs(URGENT_LOW, URGENT_HIGH + 1, size=(num_patients, 1))
-        
-        # Create wait time data
-        alpha = randint.rvs(ELAPSED_WAIT_LOW, ELAPSED_WAIT_HIGH + 1, 
-                            size=(num_patients, 1))
-        
-        # Create health status data
-        health_status = np.multiply(rho, NUM_DAYS - alpha) 
-            
-        # Create operating hours time data
-        B = np.zeros(shape=(NUM_HOSPITALS, NUM_DAYS))
-        for h in H:
-            for d in D:
-                B[h, d] = random.choice(POSSIBLE_REG_HOURS)
-        
-        # Create surgery times data
-        T = truncnorm.rvs(LEFT_TIME_CLIP, RIGHT_TIME_CLIP, loc=AVG_SURG_TIME,
-                          scale=STD_SURG_TIME, size=(num_patients, 1))
-        
-        # Create cost of opening hospital data
-        G = randint.rvs(OPEN_HOSP_LOW, OPEN_HOSP_HIGH + 1, 
-                        size=(NUM_HOSPITALS, NUM_DAYS))
-        
-        # Create cost of opening operating room data
-        F = randint.rvs(OPEN_OR_LOW, OPEN_OR_HIGH + 1, 
-                        size=(NUM_HOSPITALS, NUM_DAYS))
-        
-        # Create set of mandatory patients
-        mandatory_P = [p for p in P if health_status[p, 0] <= -HEALTH_THRESHOLD]
-        print("\tPercentage of patients that are mandatory (Expected 6-10%)", 
-              f"{100*len(mandatory_P) / num_patients}%")
-        
-        # Save patients data
-        with open(f'data/patients-{num_or}-{num_patients}.csv', 'w', newline='', 
-                  encoding='utf-8') as file:
-            writer = csv.writer(file)
-            writer.writerow(PATIENTS_HEADER)
-            for p in P:
-                data = [p, T[p, 0], rho[p, 0], alpha[p, 0], health_status[p, 0], 
-                        1 if p in mandatory_P else 0]
-                writer.writerow(data)
-                
-        # Save hospital data
-        with open(f'data/hospitals-{num_or}-{num_patients}.csv', 'w', newline='', 
-                  encoding='utf-8') as file:
-            writer = csv.writer(file)
-            writer.writerow(HOSPITAL_HEADER)
-            for h in H:
-                for d in D:
-                    data = [h, d, B[h, d], G[h, d], F[h, d]]
-                    writer.writerow(data)
+    # Create operating hours time data
+    B = np.zeros(shape=(NUM_HOSPITALS, NUM_DAYS))
+    for h in H:
+        for d in D:
+            B[h, d] = random.choice(POSSIBLE_REG_HOURS)
+    
+    # Create surgery times data
+    T = truncnorm.rvs(LEFT_TIME_CLIP, RIGHT_TIME_CLIP, loc=AVG_SURG_TIME,
+                      scale=STD_SURG_TIME, size=(num_patients, 1))
+    
+    # Create cost of opening hospital data
+    G = randint.rvs(OPEN_HOSP_LOW, OPEN_HOSP_HIGH + 1, 
+                    size=(NUM_HOSPITALS, NUM_DAYS))
+    
+    # Create cost of opening operating room data
+    F = randint.rvs(OPEN_OR_LOW, OPEN_OR_HIGH + 1, 
+                    size=(NUM_HOSPITALS, NUM_DAYS))
+    
+    # Create set of mandatory patients
+    mandatory_P = [p for p in P if health_status[p, 0] <= -HEALTH_THRESHOLD]
+    print("\tPercentage of patients that are mandatory (Expected 6-10%)", 
+          f"{100*len(mandatory_P) / num_patients}%")
+    
+    return P, mandatory_P, H, R, D, rho, alpha, health_status, B, T, G, F
 
+def main():
+    for num_or in NUM_ORS:
+        print(f"Generating data for {num_or} operating rooms")
+        for num_patients in NUM_PATIENTS:
+            print(f"\tGenerating data for {num_patients} patients")
+            
+            P, mandatory_P, H, R, D, rho, alpha, health_status, B, T, G, F = (
+                generate_data(num_patients, num_or) )
+            
+            # Save patients data
+            with open(f'data/patients-{num_or}-{num_patients}.csv', 'w', newline='', 
+                      encoding='utf-8') as file:
+                writer = csv.writer(file)
+                writer.writerow(PATIENTS_HEADER)
+                for p in P:
+                    data = [p, T[p, 0], rho[p, 0], alpha[p, 0], health_status[p, 0], 
+                            1 if p in mandatory_P else 0]
+                    writer.writerow(data)
+                    
+            # Save hospital data
+            with open(f'data/hospitals-{num_or}-{num_patients}.csv', 'w', newline='', 
+                      encoding='utf-8') as file:
+                writer = csv.writer(file)
+                writer.writerow(HOSPITAL_HEADER)
+                for h in H:
+                    for d in D:
+                        data = [h, d, B[h, d], G[h, d], F[h, d]]
+                        writer.writerow(data)
+
+if __name__ == '__main__':
+    main()
