ecac6f4db3a80d3ee7e16a58d9d60312471a45b3
MitchellJC <81349046+MitchellJC@users.noreply.github.com>
2023-09-05 10:41:26 +1000

    Implemented basic automated experiment in bakeoff.py

--- bake_off.py
+++ bake_off.py
@@ -4,28 +4,59 @@ Created on Mon Sep  4 11:19:35 2023
 
 @author: mitch
 """
+import time
+import pandas as pd
 from models import MIPScheduler, BendersLoopScheduler, BendersCallbackScheduler
 from data_gen import generate_data
 
-NUM_PATIENTS = 20
+NUM_PATIENTS = (20,)
+SEEDS = (42, 831, 316)
 NUM_OR = 5
 
-P, mand_P, H, R, D, rho, alpha, health_status, B, T, G, F = generate_data(
-    NUM_PATIENTS, NUM_OR, output_dict=True, verbose=False, seed=42)
+mip_obj_vals = []
+mip_times = []
 
-mip = MIPScheduler(P, H, R, D, G, F, B, T, rho, alpha, mand_P)
-mip.run_model()
+loop_obj_vals = []
+loop_times = []
 
-benders_loop = BendersLoopScheduler(P, H, R, D, G, F, B, T, rho, alpha, mand_P,
-                                    verbose=False, gurobi_log=False)
-benders_loop.run_model()
+callback_obj_vals = []
+callback_times = []
 
-
-benders_callback = BendersCallbackScheduler(P, H, R, D, G, F, B, T, rho, alpha, 
-                                            mand_P, verbose=False, 
-                                            gurobi_log=False)
-benders_callback.run_model()
-
-print(mip.model.objVal)
-print(benders_loop.model.objVal)
-print(benders_callback.model.objVal)
+for i in range(len(NUM_PATIENTS)):
+    num_patients = NUM_PATIENTS[i]
+    seed = SEEDS[i]
+    
+    P, mand_P, H, R, D, rho, alpha, health_status, B, T, G, F = generate_data(
+        num_patients, NUM_OR, output_dict=True, verbose=False, seed=seed)
+    
+    # MIP
+    mip = MIPScheduler(P, H, R, D, G, F, B, T, rho, alpha, mand_P, gurobi_log=False)
+    start_time = time.time()
+    mip.run_model()
+    run_time = time.time() - start_time
+    mip_obj_vals.append(mip.model.objVal)
+    mip_times.append(run_time)
+    
+    # Benders' Loop
+    benders_loop = BendersLoopScheduler(P, H, R, D, G, F, B, T, rho, alpha, mand_P,
+                                        verbose=False, gurobi_log=False)
+    start_time = time.time()
+    benders_loop.run_model()
+    run_time = time.time() - start_time
+    loop_obj_vals.append(benders_loop.model.objVal)
+    loop_times.append(run_time)
+    
+    # Benders' Callback
+    benders_callback = BendersCallbackScheduler(P, H, R, D, G, F, B, T, rho, alpha, 
+                                                mand_P, verbose=False, 
+                                                gurobi_log=False)
+    start_time = time.time()
+    benders_callback.run_model()
+    run_time = time.time() - start_time
+    callback_obj_vals.append(benders_callback.model.objVal)
+    callback_times.append(run_time)
+    
+columns = {'seed': SEEDS, 'num_patients': NUM_PATIENTS, 'MIP': mip_times, 
+           'loop': loop_times, 'callback': callback_times}
+df = pd.DataFrame(columns)
+print(df)
