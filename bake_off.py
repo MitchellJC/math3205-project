ba8fafd84a6dda63ea3dcfdae1200aa5ecabaf7f
MitchellJC <81349046+MitchellJC@users.noreply.github.com>
2023-09-27 20:14:43 +1000

    Added logging to bake_off.py!
    - Added logging for long runs, set
      FILE_OUTPUT=True to activate logging.
    
    - Added MIPGap output.
    
    - Set it such that time is only averaged
      over successfully solved instances (instances where the final time was less than or equal to the time limit).

--- bake_off.py
+++ bake_off.py
@@ -4,30 +4,46 @@ Created on Mon Sep  4 11:19:35 2023
 
 @author: mitch
 """
+import sys
 import time
 import pandas as pd
 import statistics
+from datetime import datetime
 from models import MIPScheduler, BendersLoopScheduler, BendersCallbackScheduler
 from data_gen import generate_data
-from constants import UNDERLINE, LBBD_PLUS, LBBD_1
+from constants import UNDERLINE, LBBD_PLUS, LBBD_1, TIME_LIMIT
 
 SEEDS = (42, 831, 306, 542, 1)
-NUM_PATIENTS = (40,)
+NUM_PATIENTS = (20,)
 NUM_OR = 5
 GAP = 0.00 # 0.01
+FILE_OUTPUT = True # WARNING! Set to false for use in IPython console (Spyder)
+TOL = 0.00001
+
+if FILE_OUTPUT:
+    now = datetime.now()
+    formatted_now = now.strftime("%d-%m-%Y_%H-%M-%S")
+    sys.stdout = open(f'logs/{formatted_now}.log', 'w')
+
 
 instance_nums = []
 seeds = []
 num_patients_full = []
 
+# MIP data
 mip_obj_vals = []
 mip_times = []
+mip_gaps = []
 
+# Loop data
 loop_obj_vals = []
 loop_times = []
+loop_gaps = []
 
+# Callback data
 callback_obj_vals = []
 callback_times = []
+callback_gaps = []
 
 for num_patients in NUM_PATIENTS:
     print(UNDERLINE, "\n Num patients", num_patients, UNDERLINE)
@@ -50,6 +66,7 @@ for num_patients in NUM_PATIENTS:
         run_time = time.time() - start_time
         mip_obj_vals.append(mip.model.objVal)
         mip_times.append(run_time)
+        mip_gaps.append(mip.model.MIPGap)
         
         # Benders' Loop
         print(f"Solving with Benders' loop")
@@ -62,8 +79,7 @@ for num_patients in NUM_PATIENTS:
         run_time = time.time() - start_time
         loop_obj_vals.append(benders_loop.model.objVal)
         loop_times.append(run_time)
-        # loop_obj_vals.append(0)
-        # loop_times.append(0)
+        loop_gaps.append(benders_loop.model.MIPGap)
         
         # Benders' Callback
         print(f"Solving with Benders' callback")
@@ -77,27 +93,37 @@ for num_patients in NUM_PATIENTS:
         run_time = time.time() - start_time
         callback_obj_vals.append(benders_callback.model.objVal)
         callback_times.append(run_time)
+        callback_gaps.append(benders_callback.model.MIPGap)
 
 # Time output
 print("\nTime Output", UNDERLINE)
 columns = {'instance': instance_nums, 'seed': seeds, 'num_patients': num_patients_full, 
-           'MIP': mip_times, 
-           'loop': loop_times, 'callback': callback_times}
+           'MIP': mip_times, 'loop': loop_times, 'callback': callback_times}
 df = pd.DataFrame(columns)
 print(df)
 
 # Objective output
 print("\nObjective Output", UNDERLINE)
 columns = {'instance': instance_nums, 'seed': seeds, 'num_patients': num_patients_full, 
-           'MIP': mip_obj_vals, 
-           'loop': loop_obj_vals, 'callback': callback_obj_vals}
+           'MIP': mip_obj_vals, 'loop': loop_obj_vals, 'callback': callback_obj_vals}
+df = pd.DataFrame(columns)
+print(df)
+
+# Gap output
+print("\nGap Output", UNDERLINE)
+columns = {'instance': instance_nums, 'seed': seeds, 'num_patients': num_patients_full, 
+           'MIP': mip_gaps, 'loop': loop_gaps, 'callback': callback_gaps}
 df = pd.DataFrame(columns)
 print(df)
 
 # Average time output
 print("\nAverage Time Output", UNDERLINE)
-columns = {'num_patients': NUM_PATIENTS, 'MIP': statistics.mean(mip_times),
-           'loop': statistics.mean(loop_times), 
-           'callback': statistics.mean(callback_times)}
+columns = {'num_patients': NUM_PATIENTS, 
+           'MIP': statistics.mean([t for t in mip_times if t < TIME_LIMIT]),
+           'loop': statistics.mean([t for t in loop_times if t < TIME_LIMIT]), 
+           'callback': statistics.mean([t for t in callback_times if t < TIME_LIMIT])}
 df = pd.DataFrame(columns)
 print(df)
+
+if FILE_OUTPUT:
+    sys.stdout.close()
