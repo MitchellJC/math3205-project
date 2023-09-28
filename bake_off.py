c0e165ca3cbcd6cc2bd8df1b7423b8d19736c4a0
MitchellJC <81349046+MitchellJC@users.noreply.github.com>
2023-09-28 17:16:53 +1000

    Started short draft for abstract
    - Changed title and running title

--- bake_off.py
+++ bake_off.py
@@ -14,10 +14,10 @@ from data_gen import generate_data
 from constants import UNDERLINE, LBBD_PLUS, LBBD_1, LBBD_2, TIME_LIMIT
 
 SEEDS = (42, 831, 306, 542, 1)
-NUM_PATIENTS = (5,)
+NUM_PATIENTS = (40,)
 NUM_OR = 5
 GAP = 0.00 # 0.01
-FILE_OUTPUT = False # WARNING! Set to false for use in IPython console (Spyder)
+FILE_OUTPUT = True # WARNING! Set to false for use in IPython console (Spyder)
 
 def run_model(model, obj_vals, times, gaps):
     start_time = time.time()
